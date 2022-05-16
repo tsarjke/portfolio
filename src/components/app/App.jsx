a8f15423c93d016ea247e6239d32845e02a08111
Ivan <ivantsaryok@yandex.ru>
2022-05-16 22:46:39 +0300

    add structure

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,9 +1,28 @@
-import React from "react";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-const App = () => {
-	return (
-		<h1>Start</h1>
-	);
-};
+import Header from '../header/Header';
+import About from '../about/About';
+import Main from '../main/Main';
+import Skills from '../skills/Skills';
+import Portfolio from '../portfolio/Portfolio';
+import Footer from '../footer/Footer';
+
+import './null.scss';
+
+const App = () => (
+  <Router>
+    <div className="container">
+      <Header />
+      <Routes>
+        <Route path="/*" element={<Main />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+      </Routes>
+      <Footer />
+    </div>
+  </Router>
+);
 
 export default App;
