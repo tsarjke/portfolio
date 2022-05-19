1eb5993a54725af1cf62a54b1a7b137c0879ecd5
Ivan <ivantsaryok@yandex.ru>
2022-05-19 22:46:26 +0300

    icons in footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import './footer.scss';
+import tg from './img/telegram2.svg';
+import inst from './img/instagram2.svg';
+import lin from './img/linkedin2.svg';
 
 const Footer = () => (
   <footer className="footer">
@@ -10,7 +13,23 @@ const Footer = () => (
       <button type="button" className="footer__btn">
         Send message
       </button>
-      <div className="footer__social">tg</div>
+      <div className="footer__social">
+        <div className="footer__tg">
+          <a href="https://tutor-ekaterina.ml" className="footer__link">
+            <img src={tg} alt="Telegram" />
+          </a>
+        </div>
+        <div className="footer__lin">
+          <a href="https://tutor-ekaterina.ml" className="footer__link">
+            <img src={lin} alt="Linkedin" />
+          </a>
+        </div>
+        <div className="footer__inst">
+          <a href="https://tutor-ekaterina.ml" className="footer__link">
+            <img src={inst} alt="Instagram" />
+          </a>
+        </div>
+      </div>
     </div>
   </footer>
 );
