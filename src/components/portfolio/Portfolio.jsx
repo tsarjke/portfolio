11ab1ec7e9993e43d03431c932d0fb50d8e499ad
Ivan <ivantsaryok@yandex.ru>
2022-05-21 00:16:24 +0300

    add portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,5 +1,45 @@
 import React from 'react';
+import Flickity from 'react-flickity-component'; // requires a loader
+import './portfolio.scss';
+import tutorImg from './img/tutor.jpg';
 
-const Portfolio = () => <div>Portfolio</div>;
+const Portfolio = () => (
+  <section className="portfolio">
+    <div className="portfolio__row">
+      <h2 className="portfolio__title title">Portfolio</h2>
+      {/* <div className="portfolio__examples card"> */}
+      <Flickity className="portfolio__examples">
+        <div className="card__content">
+          <h4 className="card__title">Tutor landing</h4>
+          <div className="card__img">
+            <img src={tutorImg} alt="Tutor" />
+          </div>
+          <a href="https://tutor-ekaterina.ml" className="card__link">
+            Go to the sources
+          </a>
+        </div>
+        <div className="card__content">
+          <h4 className="card__title">Tutor landing</h4>
+          <div className="card__img">
+            <img src={tutorImg} alt="Tutor" />
+          </div>
+          <a href="https://tutor-ekaterina.ml" className="card__link">
+            Go to the sources
+          </a>
+        </div>
+        <div className="card__content">
+          <h4 className="card__title">Tutor landing</h4>
+          <div className="card__img">
+            <img src={tutorImg} alt="Tutor" />
+          </div>
+          <a href="https://tutor-ekaterina.ml" className="card__link">
+            Go to the sources
+          </a>
+        </div>
+      </Flickity>
+      {/* </div> */}
+    </div>
+  </section>
+);
 
 export default Portfolio;
